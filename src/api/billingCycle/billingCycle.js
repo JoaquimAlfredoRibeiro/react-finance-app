307b1a8f10aa0a39dfe7906a709281eb04b2ab9b
JoaquimAlfredoRibeiro <joaquimalfredoribeiro@gmail.com>
2020-01-08 15:20:31 +0000

    bug fixes

--- src/api/billingCycle/billingCycle.js
+++ src/api/billingCycle/billingCycle.js
@@ -8,9 +8,9 @@ const creditSchema = new mongoose.Schema({
 
 const debtSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true },
+    value: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
     status: {
-        type: String, require: false, uppercase: true,
+        type: String, required: false, uppercase: true,
         enum: ['PAID', 'PENDING', 'SCHEDULED']
     }
 })
--- src/config/database.js
+++ src/config/database.js
@@ -2,4 +2,10 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/financesapp', { useNewUrlParser: true })
 
-//{ useUnifiedTopology: true }
+mongoose.Error.messages.general.required = "The attribute '{PATH}' is required."
+mongoose.Error.messages.Number.min =
+    "The inserted '{VALUE}' is lower than '{MIN}'."
+mongoose.Error.messages.Number.max =
+    "The inserted '{VALUE}' is higher than '{MAX}'."
+mongoose.Error.messages.String.enum =
+    "'{VALUE}' is not allowed for the atribute '{PATH}'."
